8b4b69a540267369746a891583aa5101bf44f8ff
FateEvent <fabio.venturini.1989@gmail.com>
2022-04-02 19:26:59 +0200

    piccole befane crescono

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 13:13:32 by faventur          #+#    #+#             */
-/*   Updated: 2022/04/02 17:15:24 by faventur         ###   ########.fr       */
+/*   Updated: 2022/04/02 19:17:47 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 int	main(void)
 {
 	t_program	program;
-	t_program	background;
 
 	program.mlx = mlx_init();
 	program.window = ft_new_window(program.mlx, 1980, 1080, "Hello world!");
-	program.sprite = ft_new_sprite(program.mlx, "block.xpm");
+	ft_put_background(program, "images/chessboard.xpm");
+	program.sprite = ft_new_sprite(program.mlx, "images/befana_giant.xpm");
 	program.sprite_position.x = 0;
 	program.sprite_position.y = 0;
 	mlx_put_image_to_window(program.mlx, program.window.reference,
