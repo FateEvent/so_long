681aa19de98a0957835f7723b13edd7fb3854283
FateEvent <fabio.venturini.1989@gmail.com>
2022-04-29 18:04:49 +0200

    stacks&filles

--- libft/includes/stacks.h
+++ libft/includes/stacks.h
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 13:10:47 by faventur          #+#    #+#             */
-/*   Updated: 2022/04/29 14:26:36 by faventur         ###   ########.fr       */
+/*   Updated: 2022/04/29 17:58:15 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,7 @@
 
 typedef struct s_node
 {
-	int				content;
-	int				index;
+	void			*content;
 	struct s_node	*prev;
 	struct s_node	*next;
 }					t_node;
@@ -32,16 +31,12 @@ typedef struct s_stack
 
 // Doubly linked list functions
 t_stack	*ft_stacknew(void);
-t_node	*ft_newnode(int content);
 t_stack	*ft_stackadd_top(t_stack *stack, t_node *new);
 t_stack	*ft_stackadd_bottom(t_stack *stack, t_node *new);
 t_stack	*ft_stackinsert(t_stack *stack, t_node *node, int position);
+t_node	*ft_newnode(void *content);
 void	ft_stackclear(t_stack **stack);
-int		ft_stacksize(t_stack *stack);
 void	ft_stackdisplay(t_stack *stack);
-void	ft_displayindex(t_stack *stack);
-
-void	ft_stack_init(int argc, char *argv[], t_stack *stack);
-void	ft_assign_index(t_stack *stack);
+int		ft_stacksize(t_stack *stack);
 
 #endif
