65b4f298e7b293a174030a480d1e6e3d819c7565
FateEvent <fabio.venturini.1989@gmail.com>
2022-04-29 19:40:13 +0200

    stacks pour tous

--- includes/a_star.h
+++ includes/a_star.h
@@ -1,41 +1,41 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   stacks.h                                           :+:      :+:    :+:   */
+/*   a_star.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/25 13:10:47 by faventur          #+#    #+#             */
-/*   Updated: 2022/04/29 19:38:48 by faventur         ###   ########.fr       */
+/*   Created: 2022/04/29 19:19:01 by faventur          #+#    #+#             */
+/*   Updated: 2022/04/29 19:39:34 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef STACKS_H
-# define STACKS_H
+#ifndef A_STAR_H
+# define A_STAR_H
 
-# include "libft.h"
+# include "stacks.h"
+# include "mlx_utils.h"
 
 typedef struct s_node
 {
-	void			*content;
-	struct s_node	*prev;
-	struct s_node	*next;
+	int				g;
+	int				h;
+	int				f;
+	int				index;
 }					t_node;
-
-typedef struct s_stack
-{
-	size_t			length;
-	struct s_node	*top;
-	struct s_node	*bottom;
-}					t_stack;
 
 // Doubly linked list functions
 t_stack	*ft_stacknew(void);
 t_stack	*ft_stackadd_top(t_stack *stack, t_node *new);
 t_stack	*ft_stackadd_bottom(t_stack *stack, t_node *new);
 t_stack	*ft_stackinsert(t_stack *stack, t_node *node, int position);
-t_node	*ft_newnode(void *content);
+t_node	*ft_newnode(int content);
+void	ft_stack_init(int argc, char *argv[], t_stack *stack);
 void	ft_stackclear(t_stack **stack);
+void	ft_stackdisplay(t_stack *stack);
+void	ft_stackdisplay(t_stack *stack);
+void	ft_assign_index(t_stack *stack);
+void	ft_displayindex(t_stack *stack);
 int		ft_stacksize(t_stack *stack);
 
 #endif
