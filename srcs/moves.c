2a91ba9b66f0c5367ae242c62899c601b54bf390
FateEvent <fabio.venturini.1989@gmail.com>
2022-05-08 16:39:46 +0200

    boh

--- srcs/moves.c
+++ srcs/moves.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 14:44:41 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/08 15:32:55 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/08 16:37:52 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,6 @@ void	track_ur_move_up(t_program *data, t_nme death)
 			exit(0);
 		data->map[death.pos.y - 1][death.pos.x] = 'K';
 	}
-	else if (data->map[death.pos.y][death.pos.x + 1] != '1'
-			&& data->map[death.pos.y][death.pos.x + 1] != 'C'
-			&& data->map[death.pos.y][death.pos.x + 1] != 'E')
-	{
-		data->map[death.pos.y][death.pos.x] = '0';
-		if (data->map[death.pos.y][death.pos.x + 1] == who_is_it(data->map))
-			exit(0);
-		data->map[death.pos.y][death.pos.x + 1] = 'K';
-	}
 }
 
 void	track_ur_move_left(t_program *data, t_nme death)
@@ -45,32 +36,19 @@ void	track_ur_move_left(t_program *data, t_nme death)
 			exit(0);
 		data->map[death.pos.y][death.pos.x - 1] = 'K';
 	}
-	else if (data->map[death.pos.y + 1][death.pos.x] != '1'
-			&& data->map[death.pos.y + 1][death.pos.x] != 'C'
-			&& data->map[death.pos.y + 1][death.pos.x] != 'E')
-	{
-		data->map[death.pos.y][death.pos.x] = '0';
-		if (data->map[death.pos.y + 1][death.pos.x] == who_is_it(data->map))
-			exit(0);
-		data->map[death.pos.y + 1][death.pos.x] = 'K';
-	}
 }
 
 void	move_ur_ass(t_program *data)
 {
-	t_vector	e_pos;
-	t_vector	c_pos;
 	t_nme		death;
 
 	death.pos = ft_get_x_and_y(data->map, 'K');
-	e_pos = ft_get_x_and_y(data->map, 'E');
-	c_pos = ft_get_char_pos(data->map);
-	if (death.pos.x > e_pos.x)
-		track_ur_move_left(data, death);
-	else if (death.pos.y > e_pos.y)
-		track_ur_move_up(data, death);
-	else if (death.pos.x < e_pos.x)
+	if (data->map[death.pos.y - 1][death.pos.x] == '1')
 		track_ur_move_right(data, death);
-	else if (death.pos.y < e_pos.y)
+	if (data->map[death.pos.y][death.pos.x + 1] == '1')
 		track_ur_move_down(data, death);
+	if (data->map[death.pos.y + 1][death.pos.x] == '1')
+		track_ur_move_left(data, death);
+	if (data->map[death.pos.y][death.pos.x - 1] == '1')
+		track_ur_move_up(data, death);
 }
--- srcs/moves2.c
+++ srcs/moves2.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/30 14:44:41 by faventur          #+#    #+#             */
-/*   Updated: 2022/05/08 15:29:04 by faventur         ###   ########.fr       */
+/*   Updated: 2022/05/08 16:17:37 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,15 +23,6 @@ void	track_ur_move_down(t_program *data, t_nme death)
 			exit(0);
 		data->map[death.pos.y + 1][death.pos.x] = 'K';
 	}
-	else if (data->map[death.pos.y][death.pos.x - 1] != '1'
-			&& data->map[death.pos.y][death.pos.x - 1] != 'C'
-			&& data->map[death.pos.y][death.pos.x - 1] != 'E')
-	{
-		data->map[death.pos.y][death.pos.x] = '0';
-		if (data->map[death.pos.y][death.pos.x - 1] == who_is_it(data->map))
-			exit(0);
-		data->map[death.pos.y][death.pos.x - 1] = 'K';
-	}
 }
 
 void	track_ur_move_right(t_program *data, t_nme death)
@@ -44,14 +35,5 @@ void	track_ur_move_right(t_program *data, t_nme death)
 		if (data->map[death.pos.y][death.pos.x + 1] == who_is_it(data->map))
 			mlx_destroy_window(data->mlx, data->window.reference);
 		data->map[death.pos.y][death.pos.x + 1] = 'K';
-	}
-	else if (data->map[death.pos.y + 1][death.pos.x] != '1'
-			&& data->map[death.pos.y + 1][death.pos.x] != 'C'
-			&& data->map[death.pos.y + 1][death.pos.x] != 'E')
-	{
-		data->map[death.pos.y][death.pos.x] = '0';
-		if (data->map[death.pos.y + 1][death.pos.x] == who_is_it(data->map))
-			mlx_destroy_window(data->mlx, data->window.reference);
-		data->map[death.pos.y + 1][death.pos.x] = 'K';
 	}
 }
