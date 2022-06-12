32522988793dbbaec04c01b2976c53ceae7cbc9d
FateEvent <fabio.venturini.1989@gmail.com>
2022-06-12 19:39:03 +0200

    yo

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -6,7 +6,7 @@
 /*   By: faventur <faventur@student.42mulhouse.fr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 13:52:36 by faventur          #+#    #+#             */
-/*   Updated: 2022/06/12 18:12:59 by faventur         ###   ########.fr       */
+/*   Updated: 2022/06/12 19:38:34 by faventur         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,34 +30,6 @@ int	ft_input(int key, void *param)
 	return (0);
 }
 
-static void	ft_detail_cond(t_program *program, t_update vars)
-{
-	if (program->map[vars.villain_pos.y - 1][vars.villain_pos.x] == '1'
-		&& program->map[vars.villain_pos.y - 1][vars.villain_pos.x + 1] == '1')
-	{
-		program->map[vars.villain_pos.y][vars.villain_pos.x] = '0';
-		program->map[vars.villain_pos.y][vars.villain_pos.x + 1] = 'K';
-	}
-	if (program->map[vars.villain_pos.y][vars.villain_pos.x + 1] == '1'
-		&& program->map[vars.villain_pos.y + 1][vars.villain_pos.x + 1] == '1')
-	{
-		program->map[vars.villain_pos.y][vars.villain_pos.x] = '0';
-		program->map[vars.villain_pos.y + 1][vars.villain_pos.x] = 'K';
-	}
-	if (program->map[vars.villain_pos.y + 1][vars.villain_pos.x] == '1'
-		&& program->map[vars.villain_pos.y + 1][vars.villain_pos.x - 1] == '1')
-	{
-		program->map[vars.villain_pos.y][vars.villain_pos.x] = '0';
-		program->map[vars.villain_pos.y][vars.villain_pos.x - 1] = 'K';
-	}
-	if (program->map[vars.villain_pos.y][vars.villain_pos.x - 1] == '1'
-		&& program->map[vars.villain_pos.y - 1][vars.villain_pos.x - 1] == '1')
-	{
-		program->map[vars.villain_pos.y][vars.villain_pos.x] = '0';
-		program->map[vars.villain_pos.y - 1][vars.villain_pos.x] = 'K';
-	}
-}
-
 int	ft_update(void *param)
 {
 	t_program	*program;
@@ -71,7 +43,7 @@ int	ft_update(void *param)
 	vars.v_var = sin(program->frame / 20) * 2;
 	program->frame++;
 	if (program->frame % 100 == 0)
-		ft_detail_cond(program, vars);
+		move_ur_ass(program);
 	mlx_clear_window(program->mlx, program->window.reference);
 	ft_display_map(program, (*program).map, (*program).pixies);
 	ft_invoke_char(program, program->pixies, vars.basic_pos, vars.b_var);
